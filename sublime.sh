f4aa9212fcf8885537943995cce024401e2691d9
fulutori <miyatori25@gmail.com>
2019-01-22 15:18:30 +0900

    メニュー内を日本語化

---
+++ sublime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir ~/.config/sublime-text-3/Packages/Default
+cp ~/.config/sublime-text-3/Packages/Japanize/Context.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Context.sublime-menu
+cp ~/.config/sublime-text-3/Packages/Japanize/Indentation.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Indentation.sublime-menu
+cp ~/.config/sublime-text-3/Packages/Japanize/Main.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Main.sublime-menu
+cp ~/.config/sublime-text-3/Packages/Japanize/Side\ Bar\ Mount\ Point.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Side\ Bar\ Mount\ Point.sublime-menu
+cp ~/.config/sublime-text-3/Packages/Japanize/Side\ Bar.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Side\ Bar.sublime-menu
+cp ~/.config/sublime-text-3/Packages/Japanize/Tab\ Context.sublime-menu.jp ~/.config/sublime-text-3/Packages/Default/Tab\ Context.sublime-menu
